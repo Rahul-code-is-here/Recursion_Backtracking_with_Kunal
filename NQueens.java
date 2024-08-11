1538df9d94adeb05ce47736a908e391ab003e3f0
Rahul Chauhan <rahulchauhanmalnath@gmail.com>
2024-08-11 19:01:11 +0530

    Add files via upload
    N Queen

---
+++ NQueens.java
@@ -0,0 +1,64 @@
+package Complete_Dsa.Recursion.Lec10_backtracking;
+
+public class NQueens {   //Time Complexity: O(N!)
+    public static int queens(boolean[][] board, int row){
+        //base condition\
+        if(row==board.length){
+            display(board);
+            System.out.println();
+            return 1;
+        }
+        int count=0;
+        // placing the queen and checking for every row and col
+        for (int col=0; col<board.length; col++){
+            //place the queen if it's safe
+            if(isSafe(board,row,col)){
+                board[row][col]=true;
+                // have aani niche queen ne place karo
+               count+= queens(board,row+1);
+               board[row][col]=false;
+            }
+        }
+        return count;
+    }
+    public static boolean isSafe(boolean[][] board,int row, int col){
+        // cheeck vertical, uper ni side queen
+       for (int i=0; i<row; i++){
+           if(board[i][col]){
+               return false;
+           }
+       }
+       // diagonal left
+        int maxLeft=Math.min(row,col);
+       for (int i=1; i<=maxLeft;i++){
+           if(board[row-i][col-i]){
+               return false;
+           }
+       }
+        // diagonal righht
+        int maxRight=Math.min(row,board.length-col-1);
+        for (int i=1; i<=maxRight;i++){
+            if(board[row-i][col+i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void display(boolean[][] board){
+        for (boolean[] row: board){
+            for (boolean element: row){
+                if(element){
+                    System.out.print("Q ");
+                }else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+    int n=5;
+    boolean[][] board=new boolean[n][n];
+    System.out.println(queens(board,0));
+    }
+}
